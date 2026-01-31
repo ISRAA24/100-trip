ea85e285f61a2e81b69fd33b28fb1dc497254ede
IsraaAhmed <ia5186738@gmail.com>
2026-02-01 00:05:19 +0200

    stringRepeats

--- Sum without highest and lowest number.js
+++ Sum without highest and lowest number.js
@@ -3,13 +3,13 @@
 //filter array to remove max and min values
 //sum the remaining values in the array
 function sumWithoutHighestAndLowest(arr) {
-    if (arr == null) return 0;
+    if (arr == null || arr.length < 3) return 0;
     let maxValue = Math.max(...arr);
     let minValue = Math.min(...arr);
     let filteredArr = arr.filter(x => x !== maxValue && x !== minValue);
-    let newArr = filteredArr.reduce((acc, current) => acc + current, 0);
-    console.log(newArr);
+    let sum = filteredArr.reduce((acc, current) => acc + current, 0);
+    console.log(sum);
 
 
 }
-sumWithoutHighestAndLowest([6, 2, 1, 8, 10, 13, 4, 5]);
+sumWithoutHighestAndLowest([ -6, 20, -1, 10, -12 ]);
---
+++ stringRepeat.js
@@ -0,0 +1,4 @@
+function stringRepeat(repeats,str) {
+    return str.repeat(repeats);
+}
+console.log(stringRepeat(3, " Esraa ")); 
